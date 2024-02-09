9ecb3d8243d06ac355152c3ed1932d159aab42ef
Ramesh Das <157310459+Hydra-Ramesh@users.noreply.github.com>
2024-02-10 00:06:38 +0530

    Area of Circle

---
+++ Area-of-Circle.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main(){
+    float r;
+    float pi=3.14;
+    printf("Enter the value of r :");
+    scanf("%f",&r);
+    printf("\nArea of Circle is :%f",pi*r*r);
+    return 0;
+}
