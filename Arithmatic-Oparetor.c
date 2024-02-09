786c7190d31454df47c2482148a27f583cdf7ce6
Ramesh Das <157310459+Hydra-Ramesh@users.noreply.github.com>
2024-02-10 00:07:22 +0530

    All Arithmath Operators

---
+++ Arithmatic-Oparetor.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main(){
+    int a=3;
+    int b=2;
+    printf("%d",a+b);
+    printf("\n%d",a-b);
+    printf("\n%d",a*b);
+    printf("\n%d",a/b);
+    printf("\n%d",a%b);
+    return 0;
+}
