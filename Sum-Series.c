e3ececc2e978268eca1a78bd4079e7eae58ca31e
Ramesh Das <157310459+Hydra-Ramesh@users.noreply.github.com>
2024-02-15 23:59:20 +0530

    Sum of a series

---
+++ Sum-Series.c
@@ -0,0 +1,13 @@
+//Print the sum of this series 1-2+3-4+5-6 upto n.
+#include<stdio.h>
+int main(){
+    int n;
+    printf("Enter the value of n:");
+    scanf("%d",&n);
+    int sum=0;
+    for (int i=1;i<=n;i++){
+        if(i%2!=0) sum=sum+i;
+        else sum =sum-i;
+    }printf("The sum is %d",sum);
+    return 0;
+}
