5f43fe4789524dd7c3f1767fdd396f1af8605b0a
Ramesh Das <157310459+Hydra-Ramesh@users.noreply.github.com>
2024-02-15 23:59:57 +0530

    Swap a value

---
+++ Sum.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main(){
+    int x,y;
+    printf("Enter the value of x:");
+    scanf("%d",&x);
+    printf("\nEnter the value of y:");
+    scanf("%d",&y);
+    int sum=x+y;
+    printf("\nThe sum of two number is :%d",sum);
+    return 0;
+}
