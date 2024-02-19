041b26702885977b59cedaf0c912eab6f65a8e9b
Ramesh Das <157310459+Hydra-Ramesh@users.noreply.github.com>
2024-02-19 22:30:11 +0530

    Unique array

---
+++ Uniqe-Array.c
@@ -0,0 +1,19 @@
+//Find the uniqe number in a given Array where all the elements are being repeated twice with one value being uniqe.
+#include<stdio.h>
+#include<stdbool.h>
+int main(){
+    int arr[7]={1,3,2,4,1,2,3};
+    for(int i=0;i<=6;i++){
+        bool flag=false;
+        for(int j=i+1;j<=6;j++){
+            if(arr[i]==arr[j]){
+                flag=true;
+            }
+        }
+        if(flag==false){
+            printf("%d",arr[i]);
+        }
+    }
+    return 0;
+}
+
